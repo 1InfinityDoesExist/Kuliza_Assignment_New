6917f620aa448993c31a2cbd8d2ac138223d5337
1InfinityDoesExist <patelavinash508@gmail.com>
2021-05-15 21:49:03 +0545

    authorization added

--- src/main/java/com/example/demo/KulizaAssignmentApplication.java
+++ src/main/java/com/example/demo/KulizaAssignmentApplication.java
@@ -1,15 +1,16 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class KulizaAssignmentApplication {
-	private static final Logger logger = LoggerFactory.getLogger(KulizaAssignmentApplication.class);
 
 	public static void main(String[] args) {
+		log.info("-----Starting the application.....!!!!!");
 		SpringApplication.run(KulizaAssignmentApplication.class, args);
 
 	}
